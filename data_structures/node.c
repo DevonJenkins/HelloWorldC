04c61450ad13d1358ed8302de7605e06c71004ba
DVNJNKNS <devonmjenkins3@gmail.com>
2022-10-05 23:24:30 -0400

    implement and traverse a three node linked list

---
+++ data_structures/node.c
@@ -0,0 +1,28 @@
+typedef struct node
+{
+	string phrase;
+	struct node *next; 
+}
+
+node;
+
+node *n = malloc(sizeof(node));
+
+n->phrase = "Hi!";
+n->next = NULL
+list = n;
+
+n = malloc(sizeof(node));
+
+n->phrase = "HEY!";
+n->next list;
+list = n;
+
+
+
+//make sure to call free after running malloc to free up the unused or garbage
+//data 
+//
+//
+
+
